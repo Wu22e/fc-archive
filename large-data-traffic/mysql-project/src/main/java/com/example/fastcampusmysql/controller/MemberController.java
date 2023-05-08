752de472b4f3ecb087c67de784a947b13551e431
Wu22e <gulb1602@gmail.com>
2023-05-09 00:48:24 +0900

    feat : 회원정보 조회 구현
    - jdbcTemplate 이용

--- large-data-traffic/mysql-project/src/main/java/com/example/fastcampusmysql/controller/MemberController.java
+++ large-data-traffic/mysql-project/src/main/java/com/example/fastcampusmysql/controller/MemberController.java
@@ -1,9 +1,12 @@
 package com.example.fastcampusmysql.controller;
 
+import com.example.fastcampusmysql.domain.member.dto.MemberDto;
 import com.example.fastcampusmysql.domain.member.dto.RegisterMemberCommand;
-import com.example.fastcampusmysql.domain.member.entity.Member;
+import com.example.fastcampusmysql.domain.member.service.MemberReadService;
 import com.example.fastcampusmysql.domain.member.service.MemberWriteService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,9 +15,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MemberController {
     private final MemberWriteService memberWriteService;
+    private final MemberReadService memberReadService;
 
     @PostMapping("/members")
-    public Member register(@RequestBody RegisterMemberCommand command) {
-        return memberWriteService.create(command);
+    public MemberDto register(@RequestBody RegisterMemberCommand command) {
+        var member = memberWriteService.register(command);
+        // 객체와 객체간의 맵핑하는 로직은 주로 mapper 레이어를 하나더 두기도 함.
+        // 이번 프로젝트는 사이즈가 작으므로 memberReadSize 의 매핑 메서드를 public 으로 연다.
+        return memberReadService.toDto(member);
+    }
+
+    @GetMapping("/members/{id}")
+    public MemberDto getMember(@PathVariable Long id) {
+        return memberReadService.getMember(id);
     }
 }
